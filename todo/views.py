ed83c8423da713537f9f6d1114ad453780ac22a7
Rohit Bokam <rohit.bokam97@gmail.com>
2017-05-15 02:04:29 +0530

    added urls

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the todo index.")
+
+def detail(request, task_id):
+    return HttpResponse("Hello, world. You're at the %s index." %task_id)    
+
