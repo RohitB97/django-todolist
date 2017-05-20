1eca11420611a3f1db5580f0633e337863245d93
Rohit Bokam <rohit.bokam97@gmail.com>
2017-05-20 13:16:51 +0530

    structure changes

--- todo/views.py
+++ todo/views.py
@@ -6,8 +6,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "todo/index.html")
+    return render(request, "todo/views/index.html")
 
 def detail(request, task_id):
-    return render(request,"todo/detail.html",{'task_id':task_id})    
+    return render(request,"todo/views/detail.html",{'task_id':task_id})    
 
