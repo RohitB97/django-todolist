390493e04275b4c42ab4efaa993921c4429a3f8c
Rohit Bokam <rohit.bokam97@gmail.com>
2017-05-21 20:54:31 +0530

    changed things

--- todo/static/todolist/controller.js
+++ todo/static/todolist/controller.js
@@ -1,5 +1,13 @@
-var app = angular.module("home",[]);
+/*var app = angular.module("home",[]);
 
 app.controller("homeCtrl",function($scope){
        $scope.task = {name:'nigga'};
-});
+});*/
+
+function addtask(){
+	console.log("test");
+  }
+
+function clearfield(){
+	console.log('cleared');
+};
--- todo/views.py
+++ todo/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return render(request, "todo/index.html")
+	task_list = Task.objects.all();
+	context = {'task_list':task_list}
+	return render(request,"todo/index.html",context)
 
-def detail(request, task_id):
-    return render(request,"todo/detail.html",{'task_id':task_id})    
+def detail(request,task_id):
+    task = Task.objects.get(pk=task_id)
+    context = {'task':task}
+    return render(request,"todo/detail.html",context)    
 
